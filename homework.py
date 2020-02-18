cc42e03fb463fbcd534aedcf87be251cb62188a5
汪心禾 <wangxinhe06@gmail.com>
2020-02-18 15:56:12 +0800

    Update homework.py for customized site

--- homework.py
+++ homework.py
@@ -4,16 +4,17 @@ from json import loads
 from urllib.request import urlopen, Request
 
 
+SITE = input('Site: ')
 COOKIE = 'pj=' + input('pj=')
 
 try:
-    homeworkList = loads(urlopen(Request('http://114.118.97.15/data/module/homework/all.asp?sAct=GetHomeworkListByStudent', ('iIsExam=0&iPageBegin=0&iPageCount=' + loads(urlopen(Request('http://114.118.97.15/data/module/homework/all.asp?sAct=GetHomeworkListByStudent', b'iIsExam=0', headers={'Cookie': COOKIE})).read())['iCount']).encode(), headers={'Cookie': COOKIE})).read())
+    homeworkList = loads(urlopen(Request(f'{SITE}/data/module/homework/all.asp?sAct=GetHomeworkListByStudent', ('iIsExam=0&iPageBegin=0&iPageCount=' + loads(urlopen(Request(f'{SITE}/data/module/homework/all.asp?sAct=GetHomeworkListByStudent', b'iIsExam=0', headers={'Cookie': COOKIE})).read())['iCount']).encode(), headers={'Cookie': COOKIE})).read())
     assert homeworkList['sRet'] == 'succeeded', homeworkList
     for homework in homeworkList['aHomework']:
         if not homework['sTimeFlag'] and not int(homework['iFinished']):
-            courseHomework = loads(urlopen(Request('http://114.118.97.15/data/module/course/all.asp?sAct=BeginCourseHomework', ('iHomeworkId=' + homework['iHomeworkId']).encode(), headers={'Cookie': COOKIE})).read())
+            courseHomework = loads(urlopen(Request(f'{SITE}/data/module/course/all.asp?sAct=BeginCourseHomework', ('iHomeworkId=' + homework['iHomeworkId']).encode(), headers={'Cookie': COOKIE})).read())
             assert courseHomework['sRet'] == 'succeeded', courseHomework
-            courseTrainDetails = loads(urlopen(Request('http://114.118.97.15/data/module/course/all.asp?sAct=AddCourseTrainDetails', ('iTrainId=' + courseHomework['iTrainId'] + '&iHomeworkId=' + homework['iHomeworkId'] + '&iPageId=' + courseHomework['sQuestionIds'] + '&sSerialIds=' + '0' + '&sAnswers=' + '%0D%0A%09+%09%0D%0A' + '&sSolutions=' + '%0D%0A%09+%09%0D%0A' + '&sScores=' + '100').encode(), headers={'Cookie': COOKIE})).read())
+            courseTrainDetails = loads(urlopen(Request(f'{SITE}/data/module/course/all.asp?sAct=AddCourseTrainDetails', ('iTrainId=' + courseHomework['iTrainId'] + '&iHomeworkId=' + homework['iHomeworkId'] + '&iPageId=' + courseHomework['sQuestionIds'] + '&sSerialIds=' + '0' + '&sAnswers=' + '%0D%0A%09+%09%0D%0A' + '&sSolutions=' + '%0D%0A%09+%09%0D%0A' + '&sScores=' + '100').encode(), headers={'Cookie': COOKIE})).read())
             assert courseTrainDetails['sRet'] == 'succeeded', courseTrainDetails
 except KeyboardInterrupt:
     print()
