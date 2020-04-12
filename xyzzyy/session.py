623ff00c151fb9eb78b3465cf301a39c9a8b320c
汪心禾 <wangxinhe06@gmail.com>
2020-04-12 14:50:25 +0800

    Move `class Session` to `session.py`

---
+++ xyzzyy/session.py
@@ -0,0 +1,35 @@
+from json import loads
+import re
+from urllib.parse import urlencode
+from urllib.request import urlopen, Request
+
+
+class Session:
+
+    def __init__(self, site, pj):
+        self.site = site
+        self.pj = pj
+
+        class Data:
+            def __init__(self, session):
+                self.session = session
+
+            def __getattr__(self, name):
+                match = re.fullmatch(r'([a-z]+)_([A-Za-z]+)', name)
+                if match:
+                    module, sAct = match.groups()
+
+                    def _act(**kwargs):
+                        return self.session.act(module, sAct, **kwargs)
+                    return _act
+                raise AttributeError
+        self.data = Data(self)
+
+    def act(self, module, sAct, **kwargs):
+        data = loads(urlopen(Request(
+            f'{self.site}/data/module/{module}/all.asp?sAct={sAct}',
+            urlencode(kwargs).encode(),
+            headers={'Cookie': f'pj={self.pj}'}
+        )).read())
+        assert data['sRet'] == 'succeeded', data
+        return data
