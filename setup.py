c58092d780a12f3e39bd2fa25e44d57e515669b9
汪心禾 <wangxinhe06@gmail.com>
2020-04-12 16:58:14 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="xyzzyy",
+    author="汪心禾",
+    author_email="wangxinhe06@gmail.com",
+    description="校园智障英语",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/wangxinhe2006/xyzzyy",
+    packages=['xyzzyy'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
