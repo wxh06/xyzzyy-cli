586b5dcd6ca6e6d78204223b0b4bf1f2cd504a4f
汪心禾 <wangxinhe06@gmail.com>
2020-10-06 21:54:29 +0800

    ThreadPoolExecutor

--- xyzzyy/actions.py
+++ xyzzyy/actions.py
@@ -1,11 +1,17 @@
+from concurrent.futures import ThreadPoolExecutor
+
+
 def exam(s, show_all, is_exam: bool = 1):
-    return [
-        s.data.exam_GetExamContent(iExamId=exam['sQuestionIds'])
-        for exam in s.data.homework_GetHomeworkListByStudent(
-            iIsExam=is_exam, iPageCount=s.data.homework_GetHomeworkListByStudent(iIsExam=is_exam)['iCount']
-        )['aHomework']
-        if show_all or not exam['sTimeFlag'] and not int(exam['iFinished'])
-    ]
+    with ThreadPoolExecutor() as e:
+        return [
+            future.result() for future in [
+                e.submit(s.data.exam_GetExamContent, iExamId=exam['sQuestionIds'])
+                for exam in s.data.homework_GetHomeworkListByStudent(
+                    iIsExam=is_exam, iPageCount=s.data.homework_GetHomeworkListByStudent(iIsExam=is_exam)['iCount']
+                )['aHomework']
+                if show_all or not exam['sTimeFlag'] and not int(exam['iFinished'])
+            ]
+        ]
 
 
 def word_train(s, unit):
